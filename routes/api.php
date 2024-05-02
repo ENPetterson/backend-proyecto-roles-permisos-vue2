7f1bab74f3c40f6307bf977afa21eb014d6496ea
Erick Petterson <e.n.petterson@gmail.com>
2024-05-02 20:58:53 -0300

    Clase 2 Vue 2

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,33 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PermisoController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix("v1/auth")->group(function() {
+    
+    Route::post("login", [AuthController::class, "funIngresar"]);
+    Route::post("register", [AuthController::class, "funRegistro"]);
+
+    Route::middleware('auth:sanctum')->group(function() {
+
+        Route::get("profile", [AuthController::class, "funPerfil"]);
+        Route::post("logout", [AuthController::class, "funSalir"]);
+    });
+    
+});
+
+// users
+Route::middleware('auth:sanctum')->group(function() {
+
+    Route::apiResource("users", UserController::class);
+    Route::apiResource("permiso", PermisoController::class);
+    Route::apiResource("role", RoleController::class);
 });
